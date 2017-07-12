6015ac92b999444e1a495c0aa4f26dc6c9505d71
invinc87 <invincible87@126.com>
2017-07-12 11:27:26 +0800

    Fix updating config

--- app/controllers/spree/admin/google_merchants_controller.rb
+++ app/controllers/spree/admin/google_merchants_controller.rb
@@ -3,12 +3,21 @@ module Spree
     class GoogleMerchantsController < Admin::BaseController
 
       def update
-        Spree::GoogleMerchant::Config.set(params[:preferences])
+        Spree::GoogleMerchant::Config["google_merchant_title"] = merchant_params[:google_merchant_title]
+        Spree::GoogleMerchant::Config["google_merchant_description"] = merchant_params[:google_merchant_description]
+        Spree::GoogleMerchant::Config["production_domain"] = merchant_params[:production_domain]
+
+        # Spree::GoogleMerchant::Config.set(params[:preferences])
         respond_to do |format|
           format.html { redirect_to admin_google_merchants_path }
         end
       end
 
+      private
+
+      def merchant_params
+        params[:preferences].permit(:google_merchant_title, :google_merchant_description, :production_domain)
+      end
     end
   end
 end
